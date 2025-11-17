5bd6c881f4ba726df33a1aedc1dafd62f816c951
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2025-11-17 20:17:01 +0530

    handling port issue

--- main.py
+++ main.py
@@ -262,4 +262,6 @@ def developer():
 ### python main
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
